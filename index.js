f45e42f6c4e427192bf60f9bdae6cb4f4027c980
agnesjuliana <agafordev@gmail.com>
2022-11-28 00:53:31 +0700

    vercel

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true,}));
 app.use(cors());
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 webpush.setVapidDetails('mailto:mercymeave@section.com', publicVapidKey,privateVapidKey);
 
 //subscribe route
