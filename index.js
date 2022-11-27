883f34130444a9ab30231bd3477b440393e50ac6
agnesjuliana <agafordev@gmail.com>
2022-11-28 00:11:44 +0700

    add cors

--- index.js
+++ index.js
@@ -4,32 +4,22 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const logger = require("./helpers/utils/logger");
-
-
-
+const cors = require("cors");
 
 const publicVapidKey = 'BBY2Bodhi8oGHbwXUn1ZollohsJKrsxc-BlHnG7eQsM2MIztTPE374HcWMOXe5yhYDwtTJaedQSo9f7Yvis7bb0';
 const privateVapidKey = 'Dew_wXTjwFZ-xDPOBqCX0ZTq0aGcFv8Wx-wgfY6VMyI';
 
 app.use(bodyParser.json())
-
-
-
+app.use(bodyParser.urlencoded({extended: true,}));
+app.use(cors());
 
 webpush.setVapidDetails('mailto:mercymeave@section.com', publicVapidKey,privateVapidKey);
 
 //subscribe route
 app.post('/subscribe', (req, res)=>{
-    //get push subscription object from the request
     const subscription = req.body;
-
-    //send status 201 for the request
     res.status(201).json({})
-
-    //create paylod: specified the detals of the push notification
     const payload = JSON.stringify({title: 'Section.io Push Notification' });
-
-    //pass the object into sendNotification fucntion and catch any error
     webpush.sendNotification(subscription, payload).then(res => console.log("success")).catch(err=> console.error(err));
 })
 
