4f5333cbece2b45ac9d076df5c06be93a6f1ada7
agnesjuliana <agafordev@gmail.com>
2022-11-28 00:48:55 +0700

    lagi

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ app.use(bodyParser.urlencoded({extended: true,}));
 app.use(cors());
 
 app.use(function(req, res, next) {
-    req.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-  });
+});
 
 webpush.setVapidDetails('mailto:mercymeave@section.com', publicVapidKey,privateVapidKey);
 
