916d7f1ab07cfb1f5eabefe79d3df784b51b9f88
agnesjuliana <agafordev@gmail.com>
2022-11-28 00:44:37 +0700

    aaaa

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true,}));
 app.use(cors());
 
 app.use(function(req, res, next) {
+    req.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
